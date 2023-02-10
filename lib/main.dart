e3b79e7378e7d00eecc98a90db74017818db72df
Ali Akolade <aliakolade@gmail.com>
2023-02-10 14:38:34 +0100

    version 1

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_multi_formatter/flutter_multi_formatter.dart';
 import 'package:intl/intl.dart';
+import 'package:splash_view/source/presentation/pages/pages.dart';
+import 'package:splash_view/source/presentation/widgets/done.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,9 +16,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(title: 'DSR Calculator',
+    return MaterialApp(
+        title: 'OKB Homes: DSR Calculator',
         debugShowCheckedModeBanner: false,
-        home: MyHomePage(title: 'DSR Calculator'));
+        home: SplashView(
+            backgroundColor: const Color.fromRGBO(37, 33, 34, 1),
+            logo: Image.asset('assets/dsrLogo.png'),
+            loadingIndicator: Column(children: const [
+              CircularProgressIndicator(color: Color.fromRGBO(246, 207, 96, 1)),
+              SizedBox(height: 20)
+            ]),
+            bottomLoading: true,
+            done: Done(const MyHomePage(title: 'DSR Calculator'))));
   }
 }
 
@@ -31,8 +42,15 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text(widget.title)),
-        body: const SingleChildScrollView(child: DSRForm()));
+    return Scaffold(
+        appBar: AppBar(
+            title:
+                Text(widget.title, style: const TextStyle(color: Colors.black)),
+            backgroundColor: const Color.fromRGBO(246, 207, 96, 1)),
+        body: const SingleChildScrollView(
+            child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                child: DSRForm())));
   }
 }
 
@@ -50,8 +68,8 @@ class DSRFormState extends State<DSRForm> {
   TextEditingController yearOfBirth = TextEditingController(text: "2000");
   TextEditingController yearOfEmployment = TextEditingController(text: "2023");
   TextEditingController netSalary = TextEditingController(text: "0");
-  TextEditingController mortgageAmount = TextEditingController(
-      text: "15000000");
+  TextEditingController mortgageAmount =
+      TextEditingController(text: "15000000");
   TextEditingController mortgageInterest = TextEditingController(text: "10");
   TextEditingController mortgageTenure = TextEditingController(text: "1");
   TextEditingController maxAge = TextEditingController(text: "60");
@@ -78,45 +96,57 @@ class DSRFormState extends State<DSRForm> {
   final NumberFormat usCurrency = NumberFormat('#,###.##', 'en_US');
 
   editConstants() {
-    showDialog(context: context, builder: (BuildContext context) {
-      return SimpleDialog(title: const Text('Constants'),
-        children: <Widget>[
-          TextFormField(controller: maxAge,
-              keyboardType: TextInputType.number,
-              inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-              decoration: const InputDecoration(
-                  labelText: 'Retirement Age (Yrs)'),
-              onChanged: (value) {
-                getMaxPeriod();
-              }),
-          const SizedBox(height: 10),
-          TextFormField(controller: maxService,
-              keyboardType: TextInputType.number,
-              inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-              decoration: const InputDecoration(
-                  labelText: 'Retirement Length of Service (Yrs)'),
-              onChanged: (value) {
-                getMaxPeriod();
-              }),
-          const SizedBox(height: 10),
-          TextFormField(controller: dsrLimit,
-              keyboardType: TextInputType.number,
-              inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-              decoration: const InputDecoration(
-                  labelText: 'Retirement Length of Service (Yrs)'),
-              onChanged: (value) {
-                calculateDSR();
-              }),
-
-          Row(children: [SimpleDialogOption(onPressed: () {
-            getMaxPeriod();
-            Navigator.pop(context);
-          }, child: const Text('Confirm'),), SimpleDialogOption(onPressed: () {
-            Navigator.pop(context);
-          }, child: const Text('Cancel'),)
-          ])
-        ],);
-    });
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return SimpleDialog(title: const Text('Constants'), children: <
+              Widget>[
+            Padding(
+                padding: const EdgeInsets.fromLTRB(10, 0, 10, 10),
+                child: Column(children: [
+                  TextFormField(
+                      controller: maxAge,
+                      keyboardType: TextInputType.number,
+                      inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                      decoration: const InputDecoration(
+                          labelText: 'Retirement Age (Yrs)'),
+                      onChanged: (value) {
+                        retireAge = int.parse(value) -
+                            (DateTime.now().year - int.parse(yearOfBirth.text));
+                        getMaxPeriod();
+                      }),
+                  const SizedBox(height: 10),
+                  TextFormField(
+                      controller: maxService,
+                      keyboardType: TextInputType.number,
+                      inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                      decoration: const InputDecoration(
+                          labelText: 'Retirement Length of Service (Yrs)'),
+                      onChanged: (value) {
+                        retireService = int.parse(value) -
+                            (DateTime.now().year -
+                                int.parse(yearOfEmployment.text));
+                        getMaxPeriod();
+                      }),
+                  const SizedBox(height: 10),
+                  TextFormField(
+                      controller: dsrLimit,
+                      keyboardType: TextInputType.number,
+                      inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                      decoration:
+                          const InputDecoration(labelText: 'DSR Limit (%)'),
+                      onChanged: (value) {
+                        calculateDSR();
+                      })
+                ])),
+            SimpleDialogOption(
+                onPressed: () {
+                  getMaxPeriod();
+                  Navigator.pop(context);
+                },
+                child: const Text('Close'))
+          ]);
+        });
   }
 
   calculateEMI() {
@@ -125,7 +155,7 @@ class DSRFormState extends State<DSRForm> {
         (mPeriod * noPayments));
     setState(() {
       emi = double.parse(
-          mortgageAmount.text.replaceAll("₦ ", "").replaceAll(",", "")) *
+              mortgageAmount.text.replaceAll("₦ ", "").replaceAll(",", "")) *
           ((((int.parse(mortgageInterest.text) / 100) / noPayments) * temp) /
               (temp - 1));
     });
@@ -142,9 +172,13 @@ class DSRFormState extends State<DSRForm> {
 
   calculateDSR() {
     setState(() {
-      dsr = double.parse(((emi / (double.parse(
-          netSalary.text.replaceAll("₦ ", "").replaceAll(",", "")) *
-          (12 / noPayments))) * 100).toStringAsFixed(3));
+      dsr = double.parse(((emi /
+                  (double.parse(netSalary.text
+                          .replaceAll("₦ ", "")
+                          .replaceAll(",", "")) *
+                      (12 / noPayments))) *
+              100)
+          .toStringAsFixed(3));
     });
   }
 
@@ -152,149 +186,157 @@ class DSRFormState extends State<DSRForm> {
   void initState() {
     super.initState();
     setState(() {
-      age = DateTime
-          .now()
-          .year - int.parse(yearOfBirth.text);
-      retireAge = int.parse(maxAge.text) - (DateTime
-          .now()
-          .year - int.parse(yearOfBirth.text));
-      lenService = DateTime
-          .now()
-          .year - int.parse(yearOfEmployment.text);
-      retireService = int.parse(maxService.text) - (DateTime
-          .now()
-          .year - int.parse(yearOfEmployment.text));
+      age = DateTime.now().year - int.parse(yearOfBirth.text);
+      retireAge = int.parse(maxAge.text) -
+          (DateTime.now().year - int.parse(yearOfBirth.text));
+      lenService = DateTime.now().year - int.parse(yearOfEmployment.text);
+      retireService = int.parse(maxService.text) -
+          (DateTime.now().year - int.parse(yearOfEmployment.text));
       getMaxPeriod();
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Form(key: _formKey,
-        child: Column(mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              TextFormField(controller: yearOfBirth,
-                  keyboardType: TextInputType.number,
-                  decoration: const InputDecoration(
-                      icon: Icon(Icons.person), labelText: 'Birth Year'),
-                  onChanged: (value) {
-                    setState(() {
-                      age = DateTime
-                          .now()
-                          .year - int.parse(value);
-                      retireAge = int.parse(maxAge.text) - (DateTime
-                          .now()
-                          .year - int.parse(value));
-                      getMaxPeriod();
-                    });
-                  }),
-              Row(children: [
-                const SizedBox(width: 40),
-                Text("Age = $age"),
-                const Spacer(),
-                Text("Retires in $retireAge years"),
-                const SizedBox(width: 5)
-              ]),
-              const SizedBox(height: 15),
-              TextFormField(keyboardType: TextInputType.number,
-                  controller: yearOfEmployment,
-                  decoration: const InputDecoration(
-                      icon: Icon(Icons.person), labelText: 'Employment Year'),
-                  onChanged: (value) {
-                    setState(() {
-                      lenService = DateTime
-                          .now()
-                          .year - int.parse(value);
-                      retireService = int.parse(maxService.text) - (DateTime
-                          .now()
-                          .year - int.parse(value));
-                      getMaxPeriod();
-                    });
-                  }),
-              Row(children: [
-                const SizedBox(width: 40),
-                Text("Length of Service = $lenService"),
-                const Spacer(),
-                Text("Retires in $retireService years"),
-                const SizedBox(width: 5)
-              ]),
-              const SizedBox(height: 15),
-              TextFormField(keyboardType: TextInputType.number,
-                  controller: netSalary,
-                  inputFormatters: [
-                    CurrencyInputFormatter(leadingSymbol: "₦",
-                        useSymbolPadding: true,
-                        mantissaLength: 1)
-                  ],
-                  decoration: const InputDecoration(icon: Icon(Icons.person),
-                      labelText: 'Net Salary (Monthly)'),
-                  onChanged: (text) {
-                    calculateDSR();
-                  }),
-              const SizedBox(height: 20),
-              Row(children: [
-                const SizedBox(width: 10),
-                Expanded(child: TextFormField(controller: mortgageAmount,
+    return Form(
+        key: _formKey,
+        child: Column(mainAxisAlignment: MainAxisAlignment.start, children: <
+            Widget>[
+          TextFormField(
+              controller: yearOfBirth,
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(
+                  icon: Icon(Icons.person), labelText: 'Birth Year'),
+              onChanged: (value) {
+                setState(() {
+                  age = DateTime.now().year - int.parse(value);
+                  retireAge = int.parse(maxAge.text) -
+                      (DateTime.now().year - int.parse(value));
+                  getMaxPeriod();
+                });
+              }),
+          Row(children: [
+            const SizedBox(width: 40),
+            Text("Age = $age"),
+            const Spacer(),
+            Text("Retires in $retireAge years"),
+            const SizedBox(width: 5)
+          ]),
+          const SizedBox(height: 15),
+          TextFormField(
+              keyboardType: TextInputType.number,
+              controller: yearOfEmployment,
+              decoration: const InputDecoration(
+                  icon: Icon(Icons.person), labelText: 'Employment Year'),
+              onChanged: (value) {
+                setState(() {
+                  lenService = DateTime.now().year - int.parse(value);
+                  retireService = int.parse(maxService.text) -
+                      (DateTime.now().year - int.parse(value));
+                  getMaxPeriod();
+                });
+              }),
+          Row(children: [
+            const SizedBox(width: 40),
+            Text("Length of Service = $lenService"),
+            const Spacer(),
+            Text("Retires in $retireService years"),
+            const SizedBox(width: 5)
+          ]),
+          const SizedBox(height: 15),
+          TextFormField(
+              keyboardType: TextInputType.number,
+              controller: netSalary,
+              inputFormatters: [
+                CurrencyInputFormatter(
+                    leadingSymbol: "₦",
+                    useSymbolPadding: true,
+                    mantissaLength: 1)
+              ],
+              decoration: const InputDecoration(
+                  icon: Icon(Icons.person), labelText: 'Net Salary (Monthly)'),
+              onChanged: (text) {
+                calculateDSR();
+              }),
+          const SizedBox(height: 20),
+          Row(children: [
+            const SizedBox(width: 10),
+            Expanded(
+                child: TextFormField(
+                    controller: mortgageAmount,
                     keyboardType: TextInputType.number,
                     inputFormatters: [
-                      CurrencyInputFormatter(leadingSymbol: "₦",
+                      CurrencyInputFormatter(
+                          leadingSymbol: "₦",
                           useSymbolPadding: true,
                           mantissaLength: 1)
                     ],
-                    decoration: const InputDecoration(
-                        labelText: 'Mortgage Amount'),
+                    decoration:
+                        const InputDecoration(labelText: 'Mortgage Amount'),
                     onChanged: (value) {
                       calculateEMI();
                     })),
-                const SizedBox(width: 10),
-                Expanded(child: TextFormField(controller: mortgageInterest,
+            const SizedBox(width: 10),
+            Expanded(
+                child: TextFormField(
+                    controller: mortgageInterest,
                     keyboardType: TextInputType.number,
-                    decoration: const InputDecoration(
-                        labelText: 'Interest (%)'),
+                    decoration:
+                        const InputDecoration(labelText: 'Interest (%)'),
                     onChanged: (value) {
                       calculateEMI();
                     })),
-                const SizedBox(width: 10),
-                Expanded(child: TextFormField(controller: mortgageTenure,
+            const SizedBox(width: 10),
+            Expanded(
+                child: TextFormField(
+                    controller: mortgageTenure,
                     keyboardType: TextInputType.number,
-                    decoration: const InputDecoration(
-                        labelText: 'Tenure (Yrs)'),
+                    decoration:
+                        const InputDecoration(labelText: 'Tenure (Yrs)'),
                     onChanged: (value) {
                       setState(() {
                         mPeriod = int.parse(value);
                       });
                       calculateEMI();
                     }))
-              ]),
-              const SizedBox(height: 5),
-              Row(children: [
-                const SizedBox(width: 10),
-                DropdownButton(value: interval,
-                    items: intNames.map<DropdownMenuItem<String>>((
-                        String value) {
-                      return DropdownMenuItem<String>(
-                          value: value, child: Text(value));
-                    }).toList(),
-                    onChanged: (String? value) {
-                      setState(() {
-                        interval = value!;
-                        noPayments =
-                            intPeriods.elementAt(intNames.indexOf(value));
-                      });
-                      calculateEMI();
-                    }),
-                const SizedBox(width: 30),
-                Text("Installments = ₦${usCurrency.format(emi)}")
-              ]),
-              const SizedBox(height: 30),
-              Text("DSR - $dsr%"),
-              const SizedBox(height: 5),
-              (dsr <= int.parse(dsrLimit.text)) ? const Text("Qualified") : const Text(
-                  "Not Qualified"),
-              const SizedBox(height: 20),
-              OutlinedButton(onPressed: () {
+          ]),
+          const SizedBox(height: 5),
+          Row(children: [
+            const SizedBox(width: 10),
+            DropdownButton(
+                value: interval,
+                items: intNames.map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                      value: value, child: Text(value));
+                }).toList(),
+                onChanged: (String? value) {
+                  setState(() {
+                    interval = value!;
+                    noPayments = intPeriods.elementAt(intNames.indexOf(value));
+                  });
+                  calculateEMI();
+                }),
+            const SizedBox(width: 30),
+            Text("Installments = ₦${usCurrency.format(emi)}")
+          ]),
+          const SizedBox(height: 30),
+          Text("DSR - $dsr%"),
+          const SizedBox(height: 5),
+          (dsr <= int.parse(dsrLimit.text))
+              ? const Text("Qualified",
+                  style: TextStyle(
+                      color: Colors.green, fontWeight: FontWeight.bold))
+              : const Text("Not Qualified",
+                  style: TextStyle(
+                      color: Colors.red, fontWeight: FontWeight.bold)),
+          const SizedBox(height: 20),
+          OutlinedButton(
+              onPressed: () {
                 editConstants();
-              }, child: const Text("Edit Constants"))
-            ]));
+              },
+              child: const Text("Edit Constants",
+                  style: TextStyle(
+                      color: Colors.black54, fontWeight: FontWeight.bold)))
+        ]));
   }
 }
